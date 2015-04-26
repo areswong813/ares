fdb3802eb2f5a7b913778ba45304ed9ad3c819b7
ares <areswong813@foxmail.com>
2015-04-26 16:12:07 +0800

    Update Hello.java File. Add Two Lines.

--- Hello.java
+++ Hello.java
@@ -1,5 +1,6 @@
 public class Hello {
 	public static void mian(String[] args) {
-		System.out.println("Hello World!");
+		System.out.println("Hello Ares!");
+		System.out.println("Hello AresWong813@foxmail.com!");
 	}
 }
