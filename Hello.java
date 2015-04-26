4de7b5b09fa82105a6c9b0dece07c2bbff561fa3
ares <areswong813@foxmail.com>
2015-04-26 16:30:22 +0800

    Add Demo.java

--- Hello.java
+++ Hello.java
@@ -1,6 +1,5 @@
 public class Hello {
 	public static void mian(String[] args) {
-		System.out.println("Hello Ares!");
-		System.out.println("Hello AresWong813@foxmail.com!");
+		System.out.println("Hello www.ares.cn!");
 	}
 }
