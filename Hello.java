a4b49e5737a3fe18186991121550bbeb96b6a1ef
ares <areswong813@foxmail.com>
2015-04-27 20:05:36 +0800

    modify hello.java at master

--- Hello.java
+++ Hello.java
@@ -3,5 +3,15 @@ public class Hello {
 		System.out.println("Happp New Year");
 		System.out.println("Happp New Year");
 		System.out.println("Happp New Year");
+		System.out.println("Happp New Year-4");
+		System.out.println("Happp New Year-5");
+		print();
+	}
+
+	public static void print() {
+		System.out.println("Happp New Year");
+		System.out.println("Happp New Year");
+		System.out.println("Happp New Year");
+		System.out.println("Happp New Year");
 	}
 }
